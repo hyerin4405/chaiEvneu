9ed116c4583e91489f42cf3939b4f928aaa3508d
soooxmin <113075916+soooxmin@users.noreply.github.com>
2024-03-14 11:23:15 +0900

    변경

---
+++ src/test/java/com/home/chaievneu/ChaiEvneuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.chaievneu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaiEvneuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
