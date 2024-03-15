ca5f26d1b36ca7b37413fd8111130f409114ede5
soooxmin <113075916+soooxmin@users.noreply.github.com>
2024-03-15 14:30:36 +0900

    -20240315

--- src/main/java/com/home/chaievneu/board/BoardController.java
+++ src/main/java/com/home/chaievneu/board/BoardController.java
@@ -8,9 +8,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class BoardController {
 
+
+    // 메인 페이지
+    @GetMapping("main.home")
+    public String mainPage(){
+        return "main";
+    }
+
+    // 메뉴 페이지
+    @GetMapping("menu.bo")
+    public String menuPage(){
+        return "menu";
+    }
+
+    // 브랜드 페이지
+    @GetMapping("/brand.bo")
+    public String brandPage(){
+        return "brand";
+    }
+
+    // 매장 안내 페이지
+    @GetMapping("/map.bo")
+    public String mapPage(){
+        return "map";
+    }
+
+    /**
+     * 게시판 리스트 화면
+     * @return 게시판 리스트.html
+     */
     @GetMapping("/board.bo")
     public String boardList(){
         return "board/news";
     }
+
 
 }
