f1dd9d532985fe2315fb011457914996b6dda803
soooxmin <113075916+soooxmin@users.noreply.github.com>
2024-03-18 14:27:20 +0900

    게시판 구현-20240318

--- src/main/java/com/home/chaievneu/board/BoardController.java
+++ src/main/java/com/home/chaievneu/board/BoardController.java
@@ -2,12 +2,16 @@ package com.home.chaievneu.board;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
 
     // 메인 페이지
     @GetMapping("main.home")
@@ -33,12 +37,19 @@ public class BoardController {
         return "map";
     }
 
+
+    @GetMapping("/board.write")
+    public String boardWriteForm(){
+        return "board/write";
+    }
     /**
      * 게시판 리스트 화면
      * @return 게시판 리스트.html
      */
     @GetMapping("/board.bo")
-    public String boardList(){
+    public String boardList(Model model){
+        List<Board> board = boardService.boardList();
+        model.addAttribute("board", board);
         return "board/news";
     }
 
